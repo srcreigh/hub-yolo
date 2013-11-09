76433c0689e1b6e193ae7074705112ebf48af3b3
Shane Creighton-Young <shane@tryrelay.com>
2013-11-08 23:52:12 -0500

    initial commit

---
+++ Hub/src/com/srcreigh/hub/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.srcreigh.hub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedBundleState) {
+		setContentView(R.layout.splash_activity);
+	}
+}
