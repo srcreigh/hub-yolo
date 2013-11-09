79bdb56018d2cd6f5b933fb12b206414ad2fc8d4
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2013-11-09 00:59:34 -0500

    Removed crap and added good stuff

--- Hub/src/com/srcreigh/hub/SplashActivity.java
+++ Hub/src/com/srcreigh/hub/SplashActivity.java
@@ -1,12 +1,36 @@
 package com.srcreigh.hub;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends Activity {
+	
+	private Button twitterButton;
 	
 	@Override
 	public void onCreate(Bundle savedBundleState) {
 		setContentView(R.layout.splash_activity);
+		
+		twitterButton = (Button) findViewById(R.id.connectToTwitterButton);
+		twitterButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// DO AUTH STUFF HERE
+			}
+		});
+	}
+	
+	public void onSuccessfulAuth(String twitterAuthToken) {
+		// Save the twitter token in the prefs and finish the activity with RESULT_OK
+		SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(MainActivity.TWITTER_AUTH_TOKEN, twitterAuthToken);
+	    editor.commit();
+	    
+	    setResult(Activity.RESULT_OK);
+	    finish();
 	}
 }
